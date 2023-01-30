e86e1d7b98eefbf9319dae4cc03fd04780893296
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-30 20:00:58 +0530

    Updated movie page

--- src/Page/Movie/Movie.jsx
+++ src/Page/Movie/Movie.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import Loading from '../../Components/Loading/Loading';
+import './Movie.scss'
+
 
 function Movie() {
+  const [data, setData] = useState({});
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND}/movie/${id}`).then(res => res.json());
+        setData(response.data)
+        setLoading(false)
+      }
+      catch (error) {
+        console.log('err')
+      }
+    }
+    getData()
+  }, [id])
   return (
-    <div>Movie</div>
+    <>
+      <div className='Movie'>
+        {data ?
+          <>
+            <div className='left'>
+              <img src={data.cover} alt={data.title} />
+            </div>
+            <div className='right'>
+              <span>{data.title}</span>
+            </div>
+          </> : null}
+      </div>
+      <Loading showLoading={loading} />
+    </>
   )
 }
 
