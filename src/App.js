0d47c086f51af69cb06a0dd86c04d87f1509047a
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-31 22:03:56 +0530

    Updated Navbar for Authorized users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.scss'
 import Navbar from "./Components/Navbar/Navbar";
@@ -8,10 +6,6 @@ import Login from "./Page/Login/Login";
 import Movie from "./Page/Movie/Movie";
 
 function App() {
-  const accessToken = useSelector((state) => state.accessToken);
-  useEffect(() => {
-    console.log(accessToken)
-  }, [accessToken])
   return (
     <BrowserRouter>
     <Navbar />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 
 function Navbar() {
+  const accessToken = useSelector((state) => state.accessToken);
+
   return (
     <div className='Navbar'>
         <Link to='/' className='title'>
             <span>Review</span>
             <span>Hub</span>
         </Link>
-        <Link className='login' to='/login'>Login</Link>
+        {accessToken ? <Link className='login' to='/add-movie'>Add Movie</Link> : <Link className='login' to='/login'>Login</Link>}
     </div>
   )
 }
