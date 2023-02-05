d586a8f88b74fab124519538bdc2a9c01c242305
Rahul Rajput <rahulrajput21112@gmail.com>
2023-02-05 08:52:49 +0530

    Updated Add Movie Route

--- src/Page/AddMovie/AddMovie.jsx
+++ src/Page/AddMovie/AddMovie.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './AddMovie.scss'
 import Input from './Input';
 
 function AddMovie() {
-  return (
-    <div className='AddMovie'>
-        <span>Add New Movie</span>
-        <Input placeholder={'Image URL'} />
-        <Input placeholder={'Movie Title'} />
-        <Input placeholder={'Release Year'} />
-        <Input placeholder={'Movie Description'} />
-    </div>
-  )
+    const [data, setData] = useState({
+        cover: '',
+        title: '',
+        year: '2023',
+        desc: '',
+        rating: 0
+    });
+    const [coverRead, setCoverRead] = useState(true)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+    return (
+        <div className='AddMovie'>
+            <span>Add New Movie</span>
+            <form onSubmit={handleSubmit}>
+                <Input value={data.cover} coverRead={coverRead} setCoverRead={setCoverRead} data={data} name='cover' setData={setData} placeholder={'Image URL'} />
+                <Input value={data.title} data={data} name='title' setData={setData} placeholder={'Movie Title'} />
+                <Input value={data.year} data={data} name='year' setData={setData} placeholder={'Release Year'} />
+                <Input value={data.desc} data={data} name='desc' setData={setData} placeholder={'Movie Description'} />
+                <button type='submit'>Upload</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddMovie;
--- src/Page/AddMovie/Input.jsx
+++ src/Page/AddMovie/Input.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaAngleDown } from 'react-icons/fa'
 
-function Input({placeholder, name, value}) {
-  return (
-    <input type='text' name={name} value={value} placeholder={placeholder} />
-  )
+function Input({ placeholder, name, coverRead, setCoverRead, data, setData, value }) {
+    const [show, setShow] = useState(false)
+
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    }
+
+    const handleSelect = (select) => {
+        if(select === 'URL') {
+            setCoverRead(false);
+            setShow(false)
+        }
+    }
+    
+
+    return <div className='input'>
+        {placeholder === 'Release Year' ?
+        <input type="number" onChange={handleChange} value={value} name={name} min="1900" max="2099" step="1" />
+        :
+        placeholder === 'Image URL'
+            ?
+            <>
+                <input readOnly={coverRead} type='text' onChange={handleChange} value={value} name={name} placeholder={placeholder} />
+                {show ? <div className='cover'>
+                    <span>Select from device</span>
+                    <span onClick={() => handleSelect('URL')}>Enter URL</span>
+                </div>: null}
+                <FaAngleDown className='icon' onClick={() => setShow(!show)} />
+            </>
+            :
+            <input type='text' onChange={handleChange} value={value} name={name} placeholder={placeholder} />}
+    </div>
 }
 
 export default Input
