8be97356bc17af1de9f9c92ac02d9cf1d25ef071
Rahul Rajput <rahulrajput21112@gmail.com>
2023-02-05 19:30:36 +0530

    Updated Add Movie Route

--- src/Page/AddMovie/AddMovie.jsx
+++ src/Page/AddMovie/AddMovie.jsx
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { MdClose } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { deleteLS } from '../../Function.js/DeleteLS';
 import './AddMovie.scss'
 import Input from './Input';
 
 function AddMovie() {
+    const navigate = useNavigate();
+    const accessToken = useSelector((state) => state.accessToken)
+    const [showErr, setShowErr] = useState(false);
+    const [mess, setMess] = useState('');
+    const [uploadingImage, setUploadingImage] = useState(false)
     const [data, setData] = useState({
         cover: '',
         title: '',
@@ -10,23 +20,79 @@ function AddMovie() {
         desc: '',
         rating: 0
     });
-    const [coverRead, setCoverRead] = useState(true)
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        if (uploadingImage) {
+            setShowErr(true)
+            setMess('Please wait, uploading image.')
+        }
+        else if (!data.cover) {
+            setShowErr(true)
+            setMess('Please upload image or enter image url.')
+        }
+        else if (!data.title) {
+            setShowErr(true)
+            setMess('Please enter title.')
+        }
+        else if (!data.desc) {
+            setShowErr(true)
+            setMess('Please enter description.')
+        }
+        else if (!data.year) {
+            setShowErr(true)
+            setMess('Please enter release date.')
+        }
+        else if (!uploadingImage && data.cover && data.title && data.year && data.desc) {
+            setShowErr(true)
+                    setMess('Adding, please wait.')
+            try {
+                const response = await axios.post(`${process.env.REACT_APP_BACKEND}/upload`,
+                    {
+                        cover: data.cover,
+                        title: data.title,
+                        year: data.year,
+                        desc: data.desc,
+                        rating: data.rating
+                    },
+                    {
+                        headers: {
+                            "access-token": accessToken
+                        }
+                    });
+                if (response.data.message === 'Successfully Added...') {
+                    setShowErr(true)
+                    setMess('Successfully Added')
+                }
+
+            }
+            catch (err) {
+                if (err.response.data.message === 'Unauthorized!' || err.response.data.message === 'No Token Provided!') {
+                    deleteLS();
+                    navigate('/login')
+                }
+                setShowErr(true)
+                setMess('Failed, please try again.')
+            }
+        }
     }
 
-    useEffect(() => {
-        console.log(data)
-    }, [data])
+
     return (
         <div className='AddMovie'>
             <span>Add New Movie</span>
             <form onSubmit={handleSubmit}>
-                <Input value={data.cover} coverRead={coverRead} setCoverRead={setCoverRead} data={data} name='cover' setData={setData} placeholder={'Image URL'} />
-                <Input value={data.title} data={data} name='title' setData={setData} placeholder={'Movie Title'} />
-                <Input value={data.year} data={data} name='year' setData={setData} placeholder={'Release Year'} />
-                <Input value={data.desc} data={data} name='desc' setData={setData} placeholder={'Movie Description'} />
+                <Input setShowErr={setShowErr} setUploadingImage={setUploadingImage} setMess={setMess} value={data.cover} data={data} name='cover' setData={setData} placeholder={'Image URL'} />
+                <Input setShowErr={setShowErr} setMess={setMess} value={data.title} data={data} name='title' setData={setData} placeholder={'Movie Title'} />
+                <Input setShowErr={setShowErr} setMess={setMess} value={data.year} data={data} name='year' setData={setData} placeholder={'Release Year'} />
+                <Input setShowErr={setShowErr} setMess={setMess} value={data.desc} data={data} name='desc' setData={setData} placeholder={'Movie Description'} />
                 <button type='submit'>Upload</button>
+                <span className={`error ${showErr ? 'showErr' : ''}`}>
+                    {showErr ? <>
+                        {mess}
+                        <MdClose onClick={() => setShowErr(false)} className='icon' />
+
+                    </> : ''}
+                </span>
             </form>
         </div>
     )
--- src/Page/AddMovie/Input.jsx
+++ src/Page/AddMovie/Input.jsx
@@ -1,37 +1,90 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useRef, useState } from 'react'
 import { FaAngleDown } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { deleteLS } from '../../Function.js/DeleteLS'
 
-function Input({ placeholder, name, coverRead, setCoverRead, data, setData, value }) {
+function Input({ placeholder, name, setShowErr, setMess, data, setData, value, setUploadingImage }) {
+    const navigate = useNavigate();
+    const accessToken = useSelector((state) => state.accessToken)
     const [show, setShow] = useState(false)
 
     const handleChange = (e) => {
+        setShowErr(false);
+        setMess('');
         setData({ ...data, [e.target.name]: e.target.value })
     }
 
-    const handleSelect = (select) => {
-        if(select === 'URL') {
-            setCoverRead(false);
-            setShow(false)
+    const inputref = useRef(null)
+
+    const handleInputCLick = () => {
+        inputref.current.click();
+    }
+
+    const handleImage = (e) => {
+        let file = e.target.files[0];
+        setFileToBase(file);
+        e.target.value = ''
+    }
+
+    const setFileToBase = (file) => {
+        if (!file) {
+            alert('Please select an image');
+        }
+        else {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => {
+                setShow(false);
+                setShowErr(true)
+                setMess('Please wait, uploading image.');
+                setUploadingImage(true)
+                axios.post(`${process.env.REACT_APP_BACKEND}/image`,
+                    {
+                        image: reader.result
+                    },
+                    {
+                        headers: {
+                            "access-token": accessToken
+                        }
+                    })
+                    .then((response) => {
+                        setShowErr(true)
+                        setMess('Successfully Uploaded.');
+                        setUploadingImage(false)
+                        setData({ ...data, cover: response.data.url })
+                    })
+                    .catch((err) => {
+                        if (err.response.data.message === 'Unauthorized!' || err.response.data.message === 'No Token Provided!') {
+                            deleteLS();
+                            navigate('/login')
+                          }
+                        setShowErr(true)
+                        setMess('Failed, please try again.');
+                        setUploadingImage(false)
+                    })
+            }
         }
     }
-    
+
 
     return <div className='input'>
         {placeholder === 'Release Year' ?
-        <input type="number" onChange={handleChange} value={value} name={name} min="1900" max="2099" step="1" />
-        :
-        placeholder === 'Image URL'
-            ?
-            <>
-                <input readOnly={coverRead} type='text' onChange={handleChange} value={value} name={name} placeholder={placeholder} />
-                {show ? <div className='cover'>
-                    <span>Select from device</span>
-                    <span onClick={() => handleSelect('URL')}>Enter URL</span>
-                </div>: null}
-                <FaAngleDown className='icon' onClick={() => setShow(!show)} />
-            </>
+            <input type="number" onChange={handleChange} value={value} name={name} min="1900" max="2099" step="1" />
             :
-            <input type='text' onChange={handleChange} value={value} name={name} placeholder={placeholder} />}
+            placeholder === 'Image URL'
+                ?
+                <>
+                    <input type='url' onChange={handleChange} value={value} name={name} placeholder={placeholder} />
+                    {show ? <div onClick={handleInputCLick} className='cover'>
+                        <span>Select from device</span>
+                    </div> : null}
+                    <FaAngleDown className='icon' onClick={() => setShow(!show)} />
+                    <input type='file' accept='image/*' ref={inputref} onChange={handleImage} className='file' />
+                </>
+                :
+                <input type='text' onChange={handleChange} value={value} name={name} placeholder={placeholder} />}
     </div>
 }
 
